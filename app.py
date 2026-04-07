2ddbc885268563fc030f05a343ca84687b4514b7
chass55231-byte <chass.55231@gmail.com>
2026-04-07 16:12:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,3 @@ def home():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
-    
-
-from flask import Flask, jsonify, request
-import os
-
